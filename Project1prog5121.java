c01b0f6edc91ce860c403f2f087a197d5b0813e1
marialepota <st10380003@rcconnect.edu.za>
2026-04-19 21:01:08 +0200

    Add files via upload

---
+++ Project1prog5121.java
@@ -0,0 +1,35 @@
+
+}/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1prog5121
+/**
+ *
+ * @author M Lepota
+ */
+
+
+    public static void main(String[] args) {
+        package com.mycompany.project1prog;
+
+public class Login {
+
+    private String username;
+    private String password;
+    private String cellphone;
+
+    public Login(String username, String password, String cellphone) {
+        this.username = username;
+        this.password = password;
+        this.cellphone = cellphone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
